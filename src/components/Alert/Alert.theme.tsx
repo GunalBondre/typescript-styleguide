27b96aa71d6e12d190fa28ed3295e615083851c2
gunal bondre <bondre.gunal@gmail.com>
2022-08-24 19:43:05 +0530

    Revert "deleted unused file"
    This reverts commit ef8adb6fe3dedd51d0cf17e705b8d8203caa2476.

---
+++ src/components/Alert/Alert.theme.tsx
@@ -0,0 +1,8 @@
+import { AlertTypes } from './Alert.types';
+import { Palette } from '../../themes/palette';
+
+export const AlertTheme = {
+	[AlertTypes.SUCCESS]: {
+		background: Palette.green200,
+	},
+};
